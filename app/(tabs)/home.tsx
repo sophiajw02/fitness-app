138414b8b2786fd4b4bd36684c0537e4363436e1
allencao1 <156494653+allencao1@users.noreply.github.com>
2024-05-18 20:32:59 -0400

    working on home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,7 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView, Image, Alert } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Link, router } from 'expo-router';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { icons } from '../../constants';
+import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
 
 const Home = () => {
+  const getWorkouts = async () => {
+    try {
+      const response = await axios.post('http://localhost:5050/users/', form);
+      router.replace('/home');
+      Alert.alert('Success', `User with the name ${form.fullName} and username ${form.username} added to DB!`);
+    } catch (error) {
+      Alert.alert('Error', error.message);
+      console.error(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
   return (
     <View>
       <Text>Home</Text>
